b494571d80b1ec92d4c32ec787cf8d581402c380
arosete <alexrosete12@gmail.com>
2023-02-22 14:24:36 -0800

    Adding the Recipe Search Model, partly resolves #1

--- Whats4Dinner/API/models.py
+++ Whats4Dinner/API/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class Recipe_Search(models.Model):
+    Recipe_Name = models.CharField(max_length=50)
+    Image=models.ImageField
+    Ingrediants=models.CharField(max_length=300)
+    Meal_Type=models.CharField(max_length=20)
+    Health_type=models.CharField(max_length=40)
+    Diet=models.CharField(max_length=40)
+    Calories=models.IntegerField()
+    Time=models.IntegerField()
+    
