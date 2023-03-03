71ca39830c47be40a1365fb1dbe2f1a4ad8a6c4c
arose510 <98431893+arose510@users.noreply.github.com>
2023-03-03 13:31:40 -0800

    Comment out Issue

--- Whats4Dinner/API/views.py
+++ Whats4Dinner/API/views.py
@@ -32,41 +32,41 @@ def about(request):
 #         endpoint+"?type=public"+"&q="+query+"&app_id="+app_id+"&app_key="+secret_key)
 #     print(url)
 
-def search(request):
-    if request.method == "POST":
-        search_api(request)
-    return render(request, "API/search.html")
 
 @login_required(login_url='/login/')
 def userprofile(request):
     return render(request, "API/userprofile.html")
 
 
-def search_api(request):
-    print(request.body)
-    endpoint = "https://api.edamam.com/api/recipes/v2"
-    query = request.body
-    app_id = config("RECPIE_APPLICATION_ID")
-    secret_key = config("RECIPE_SEARCH_API_KEY")
-    # https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=f93954c5&app_key=76167920eae1f7ffb1d134d573e1b9a0%20
-    url = urllib.parse.quote_plus(
-        endpoint+"?type=public"+"&q="+query+"&app_id="+app_id+"&app_key="+secret_key)
-    print(url)
+# def search_api(request):
+#     print(request.body)
+#     endpoint = "https://api.edamam.com/api/recipes/v2"
+#     query = request.body
+#     app_id = config("RECPIE_APPLICATION_ID")
+#     secret_key = config("RECIPE_SEARCH_API_KEY")
+#     # https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=f93954c5&app_key=76167920eae1f7ffb1d134d573e1b9a0%20
+#     url = urllib.parse.quote_plus(
+#         endpoint+"?type=public"+"&q="+query+"&app_id="+app_id+"&app_key="+secret_key)
+#     print(url)
+
+def search(request):
     if (request.method== "POST"):
         form = RecipeSearchForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['Recipe_Name']
             text2 = form.cleaned_data['Ingrediants']
-            text3 = form.cleaned_data['Meal_Type']   
-            text4 = form.cleaned_data['Diet']
-            text5 = form.cleaned_data['Calories']
-            text6 = form.cleaned_data['Time']
+            text3 = form.cleaned_data['Meal_Type'] 
+            text4 = form.cleaned_data['Health_Type']
+            text5 = form.cleaned_data['Diet']
+            text6= form.cleaned_data['Calorie_Range']
+            text7 = form.cleaned_data['Max_Amount_Of_Time']
             print(text)  
             print(text2)  
             print(text3)  
             print(text4)
-            print(text5)  
-            print(text6)
+            print(text5)
+            print(text6)  
+            print(text7)
         context = {
             "form_data": RecipeSearchForm,
             "Recipe_Name": text,
