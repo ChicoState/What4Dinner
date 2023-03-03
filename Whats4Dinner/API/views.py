8cf12cb45374af65b0bd3d6df51e0699eaf6b314
arose510 <98431893+arose510@users.noreply.github.com>
2023-03-02 19:11:14 -0800

    Added Diet, Calorie, Time to Search Querry

--- Whats4Dinner/API/views.py
+++ Whats4Dinner/API/views.py
@@ -13,6 +13,16 @@ def home(request):
 def about(request):
     return render(request, "API/about.html")
 
+# def search_api(request):
+#     print(request.body)
+#     endpoint = "https://api.edamam.com/api/recipes/v2"
+#     query = request.body
+#     app_id = config("RECPIE_APPLICATION_ID")
+#     secret_key = config("RECIPE_SEARCH_API_KEY")
+#     # https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=f93954c5&app_key=76167920eae1f7ffb1d134d573e1b9a0%20
+#     url = urllib.parse.quote_plus(
+#         endpoint+"?type=public"+"&q="+query+"&app_id="+app_id+"&app_key="+secret_key)
+#     print(url)
 
 def search(request):
     if (request.method== "POST"):
@@ -43,7 +53,7 @@ def search(request):
 
     else:
         context = {
-            "form_data": RecipeSearchForm()
+            "form_data": RecipeSearchForm
         }
         return render(request, "API/search.html", context)
     
