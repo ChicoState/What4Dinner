ee51c0a5ae034232951b16e6a00b5ba71bdd6b46
arose510 <98431893+arose510@users.noreply.github.com>
2023-03-02 17:51:39 -0800

    Added Diet, Calorie, Time to Search Querry

--- Whats4Dinner/API/models.py
+++ Whats4Dinner/API/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 from django.db.models import Model
 
+MEAL_TYPE = [
+    ('breakfast', 'Breakfast'),
+    ('lunch', 'Lunch'),
+    ('dinner', 'Dinner')
+]
+
+DIET_TYPE = [
+    ('balanced', 'Balanced'),
+    ('high-fiber', 'High-Fiber'),
+    ('high-protein', 'High-Protein'),
+    ('low-carb', 'Low-Carb'),
+    ('low-fat', 'Low-Fat'),
+    ('low-sodium', 'Low-Sodium'),
+]
+
 
 # Create your models here.
 class Recipe_Search(models.Model):
     Recipe_Name = models.CharField(max_length=50)
     Image=models.ImageField
     Ingrediants=models.CharField(max_length=300)
-    Meal_Type=models.CharField(max_length=20)
+    Meal_Type=models.CharField(choices=MEAL_TYPE, max_length=20)
     Health_type=models.CharField(max_length=40)
-    Diet=models.CharField(max_length=40)
+    Diet=models.CharField(choices=DIET_TYPE,max_length=40)
     Calories=models.IntegerField()
     Time=models.IntegerField()
     
--- Whats4Dinner/API/views.py
+++ Whats4Dinner/API/views.py
@@ -17,20 +17,29 @@ def search(request):
             text = form.cleaned_data['Recipe_Name']
             text2 = form.cleaned_data['Ingrediants']
             text3 = form.cleaned_data['Meal_Type']   
+            text4 = form.cleaned_data['Diet']
+            text5 = form.cleaned_data['Calories']
+            text6 = form.cleaned_data['Time']
             print(text)  
             print(text2)  
             print(text3)  
+            print(text4)
+            print(text5)  
+            print(text6)
         context = {
             "form_data": RecipeSearchForm,
             "Recipe_Name": text,
             "Ingrediants": text2,
-            "Meal_Type": text3
+            "Meal_Type": text3,
+            "Diet": text4,
+            "Calories": text5,
+            "Time": text6
         }
         return render(request, "API/search.html", context)
 
     else:
         context = {
-            "form_data": RecipeSearchForm
+            "form_data": RecipeSearchForm()
         }
         return render(request, "API/search.html", context)
     
