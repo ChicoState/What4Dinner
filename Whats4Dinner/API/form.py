46b5554a8e008b24c6d9d4ad21f37ceaba35b523
Jake West <jswest95@yahoo.com>
2023-03-22 13:29:36 -0700

    Only Recipe name is required for search, sorts by recipe name. Resolved #18

--- Whats4Dinner/API/form.py
+++ Whats4Dinner/API/form.py
@@ -5,19 +5,22 @@ from django.contrib.auth.models import User
 
 class RecipeSearchForm(forms.Form):
     Recipe_Name = forms.CharField(widget=forms.TextInput())
-    Number_Of_Ingredients = forms.CharField(widget=forms.TextInput())
+    Number_Of_Ingredients = forms.CharField(
+        widget=forms.TextInput(), required=False)
     Meal_Type = forms.CharField(
-        label='Meal Type', widget=forms.Select(choices=MEAL_TYPE))
+        label='Meal Type', widget=forms.Select(choices=MEAL_TYPE), required=False)
     Health_Type = forms.CharField(
-        label='Health Type', widget=forms.Select(choices=HEALTH_TYPE))
+        label='Health Type', widget=forms.Select(choices=HEALTH_TYPE), required=False)
     Diet = forms.CharField(
-        label='Diet Type', widget=forms.Select(choices=DIET_TYPE))
-    Calorie_Range = forms.CharField(widget=forms.TextInput())
-    Max_Amount_Of_Time = forms.IntegerField(widget=forms.NumberInput())
+        label='Diet Type', widget=forms.Select(choices=DIET_TYPE), required=False)
+    Calorie_Range = forms.CharField(widget=forms.TextInput(), required=False)
+    Max_Amount_Of_Time = forms.IntegerField(
+        widget=forms.NumberInput(), required=False)
 
 
 class SignUpForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta():
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password')
--- Whats4Dinner/API/models.py
+++ Whats4Dinner/API/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from django.db.models import Model
 
 MEAL_TYPE = [
+    ("", "None"),
     ('breakfast', 'Breakfast'),
     ('lunch', 'Lunch'),
     ('dinner', 'Dinner')
 ]
 
 DIET_TYPE = [
+    ("", "None"),
     ('balanced', 'Balanced'),
     ('high-fiber', 'High-Fiber'),
     ('high-protein', 'High-Protein'),
@@ -17,60 +18,63 @@ DIET_TYPE = [
 ]
 
 HEALTH_TYPE = [
-    ('alcohol-cocktail','Alcohol-Cocktail'),
-    ('alcohol-free','Alcohol-Free'),
-    ('celery-free','Celery-Free'),
-    ('crustacean-free','Crustacean-Free'),
-    ('dairy-free','Dairy-Free'),
-    ('dash','DASH'),
-    ('egg-free','Egg-Free'),
-    ('fish-free','Fish-Free'),
-    ('foodmap-free','Foodmap-Free'),
-    ('gluten-free','Gluten-Free'),
-    ('immuno-supportive','Immuno-Supportive'),
-    ('keto-friendly','Keto-Friendly'),
-    ('kidney-friendly','Kidney-Friendly'),
-    ('kosher','Kosher'),
-    ('low-fat-abs','Low-Fat-Abs'),
-    ('low-potassium','Low-Potassium'),
-    ('low-sugar','Low-Sugar'),
-    ('lupine-free','Lupine-Free'),
-    ('mediterranean','Mediterranean'),
-    ('mollusk-free','Mollusk-Free'),
-    ('mustard-free','Mustard-Free'),
-    ('no-oil-added','No-Oil-Added'),
-    ('paleo','Paleo'),
-    ('peanut-free','Peanut-Free'),
-    ('pescatarian','Pescatarian'),
-    ('pork-free','Pork-Free'),
-    ('red-meat-free','Red-Meat-Free'),
-    ('sesame-free','Sesame-Free'),
-    ('shellfish-free','Shellfish-Free'),
-    ('soy-free','Soy-Free'),
-    ('sugar-conscious','Sugar-Concious'),
-    ('sulfite-free','Sulfite-Free'),
-    ('tree-nut-free','Tree-Nut-Free'),
-    ('vegan','Vegan'),
-    ('vegetarian','Vegetarian'),
-    ('wheat-free','Wheat-Free'),
+    ("", "None"),
+    ('alcohol-cocktail', 'Alcohol-Cocktail'),
+    ('alcohol-free', 'Alcohol-Free'),
+    ('celery-free', 'Celery-Free'),
+    ('crustacean-free', 'Crustacean-Free'),
+    ('dairy-free', 'Dairy-Free'),
+    ('dash', 'DASH'),
+    ('egg-free', 'Egg-Free'),
+    ('fish-free', 'Fish-Free'),
+    ('foodmap-free', 'Foodmap-Free'),
+    ('gluten-free', 'Gluten-Free'),
+    ('immuno-supportive', 'Immuno-Supportive'),
+    ('keto-friendly', 'Keto-Friendly'),
+    ('kidney-friendly', 'Kidney-Friendly'),
+    ('kosher', 'Kosher'),
+    ('low-fat-abs', 'Low-Fat-Abs'),
+    ('low-potassium', 'Low-Potassium'),
+    ('low-sugar', 'Low-Sugar'),
+    ('lupine-free', 'Lupine-Free'),
+    ('mediterranean', 'Mediterranean'),
+    ('mollusk-free', 'Mollusk-Free'),
+    ('mustard-free', 'Mustard-Free'),
+    ('no-oil-added', 'No-Oil-Added'),
+    ('paleo', 'Paleo'),
+    ('peanut-free', 'Peanut-Free'),
+    ('pescatarian', 'Pescatarian'),
+    ('pork-free', 'Pork-Free'),
+    ('red-meat-free', 'Red-Meat-Free'),
+    ('sesame-free', 'Sesame-Free'),
+    ('shellfish-free', 'Shellfish-Free'),
+    ('soy-free', 'Soy-Free'),
+    ('sugar-conscious', 'Sugar-Concious'),
+    ('sulfite-free', 'Sulfite-Free'),
+    ('tree-nut-free', 'Tree-Nut-Free'),
+    ('vegan', 'Vegan'),
+    ('vegetarian', 'Vegetarian'),
+    ('wheat-free', 'Wheat-Free'),
 ]
 
 
 # Create your models here.
 class Recipe_Search(models.Model):
     Recipe_Name = models.CharField(max_length=50)
-    Image=models.ImageField
-    Ingrediants=models.CharField(max_length=300)
-    Meal_Type=models.CharField(choices=MEAL_TYPE, max_length=20)
-    Health_Type=models.CharField(choices=HEALTH_TYPE, max_length=40)
-    Diet=models.CharField(choices=DIET_TYPE,max_length=40)
-    Calories=models.CharField(max_length=50)
-    Time=models.IntegerField()
-    
+    Image = models.ImageField
+    Ingrediants = models.CharField(max_length=300, null=True)
+    Meal_Type = models.CharField(choices=MEAL_TYPE, max_length=20, null=True)
+    Health_Type = models.CharField(
+        choices=HEALTH_TYPE, max_length=40, null=True)
+    Diet = models.CharField(choices=DIET_TYPE, max_length=40, null=True)
+    Calories = models.CharField(max_length=50, null=True)
+    Time = models.IntegerField(null=True)
+
+
 class User(models.Model):
-    name=models.CharField(max_length=50)
-    email=models.EmailField(max_length=254)
-    password=models.CharField(max_length=50)
-    like_recipe=models.URLField(max_length=200)
-    experience=models.CharField(max_length=500)
-    calories_count=models.IntegerField()
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=50)
+    like_recipe = models.URLField(max_length=200)
+    experience = models.CharField(max_length=500)
+    calories_count = models.IntegerField()
