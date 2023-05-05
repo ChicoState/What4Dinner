36032301ef5df7c04e25ec3c005e5e4b3a10525c
mkchao <msaechao@csuchico.edu>
2023-05-04 21:12:35 -0700

    user branch

--- Whats4Dinner/API/models.py
+++ Whats4Dinner/API/models.py
@@ -1,15 +1,20 @@
+'''
+Model declarations
+'''
 from django.db import models
 from django.db.models import Model
 from django.contrib.auth.models import User
 from PIL import Image
 
 MEAL_TYPE = [
+    ("", "None"),
     ('breakfast', 'Breakfast'),
     ('lunch', 'Lunch'),
     ('dinner', 'Dinner')
 ]
 
 DIET_TYPE = [
+    ("", "None"),
     ('balanced', 'Balanced'),
     ('high-fiber', 'High-Fiber'),
     ('high-protein', 'High-Protein'),
@@ -18,17 +23,74 @@ DIET_TYPE = [
     ('low-sodium', 'Low-Sodium'),
 ]
 
+HEALTH_TYPE = [
+    ("", "None"),
+    ('alcohol-cocktail', 'Alcohol-Cocktail'),
+    ('alcohol-free', 'Alcohol-Free'),
+    ('celery-free', 'Celery-Free'),
+    ('crustacean-free', 'Crustacean-Free'),
+    ('dairy-free', 'Dairy-Free'),
+    ('dash', 'DASH'),
+    ('egg-free', 'Egg-Free'),
+    ('fish-free', 'Fish-Free'),
+    ('foodmap-free', 'Foodmap-Free'),
+    ('gluten-free', 'Gluten-Free'),
+    ('immuno-supportive', 'Immuno-Supportive'),
+    ('keto-friendly', 'Keto-Friendly'),
+    ('kidney-friendly', 'Kidney-Friendly'),
+    ('kosher', 'Kosher'),
+    ('low-fat-abs', 'Low-Fat-Abs'),
+    ('low-potassium', 'Low-Potassium'),
+    ('low-sugar', 'Low-Sugar'),
+    ('lupine-free', 'Lupine-Free'),
+    ('mediterranean', 'Mediterranean'),
+    ('mollusk-free', 'Mollusk-Free'),
+    ('mustard-free', 'Mustard-Free'),
+    ('no-oil-added', 'No-Oil-Added'),
+    ('paleo', 'Paleo'),
+    ('peanut-free', 'Peanut-Free'),
+    ('pescatarian', 'Pescatarian'),
+    ('pork-free', 'Pork-Free'),
+    ('red-meat-free', 'Red-Meat-Free'),
+    ('sesame-free', 'Sesame-Free'),
+    ('shellfish-free', 'Shellfish-Free'),
+    ('soy-free', 'Soy-Free'),
+    ('sugar-conscious', 'Sugar-Concious'),
+    ('sulfite-free', 'Sulfite-Free'),
+    ('tree-nut-free', 'Tree-Nut-Free'),
+    ('vegan', 'Vegan'),
+    ('vegetarian', 'Vegetarian'),
+    ('wheat-free', 'Wheat-Free'),
+]
+
 
 # Create your models here.
-class Recipe_Search(models.Model):
+class RecipeSearch(models.Model):
+    '''
+    Recpe search model declaration
+    '''
     Recipe_Name = models.CharField(max_length=50)
-    Image=models.ImageField
-    Ingrediants=models.CharField(max_length=300)
-    Meal_Type=models.CharField(choices=MEAL_TYPE, max_length=20)
-    Health_type=models.CharField(max_length=40)
-    Diet=models.CharField(choices=DIET_TYPE,max_length=40)
-    Calories=models.IntegerField()
-    Time=models.IntegerField()
+    Image = models.ImageField
+    Ingrediants = models.CharField(max_length=300, null=True)
+    Meal_Type = models.CharField(choices=MEAL_TYPE, max_length=20, null=True)
+    Health_Type = models.CharField(
+        choices=HEALTH_TYPE, max_length=40, null=True)
+    Diet = models.CharField(choices=DIET_TYPE, max_length=40, null=True)
+    Calories = models.CharField(max_length=50, null=True)
+    Time = models.IntegerField(null=True)
+    # Added field for shareable link
+    shareable_link = models.URLField(max_length=200, unique=True)
+
+class Create_Recipe(models.Model):
+    Create_RecipeName = models.CharField(null=False,max_length=50)
+    Create_Ingrediants=models.CharField(null=False,max_length=300)
+    Create_Meal_Type=models.CharField(choices=MEAL_TYPE, max_length=20, null=False)
+    Create_Health_Type=models.CharField(choices=HEALTH_TYPE, max_length=40, null=False)
+    Create_Diet=models.CharField(choices=DIET_TYPE, max_length=40, null=False)
+    Create_Calories=models.CharField(null=False, max_length=50)
+    Create_Time=models.CharField(null=False, max_length=50)
+    Create_Instruct = models.CharField(null=False, max_length=10000, default='')
+    Upload_Image=models.ImageField(null=True, blank=True, upload_to="Uploads/")
 
 
 
